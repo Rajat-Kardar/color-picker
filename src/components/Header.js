970b92aac3cec99de15ccf28950d9c0e824b7b97
Kavuk <kavita.kardar@gmail.com>
2025-03-04 23:34:08 +0530

    color picker setup

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div className="bg-gray-700">
+      <p className="text-center text-red-400">ColorPicker</p>
+    </div>
+  );
+};
